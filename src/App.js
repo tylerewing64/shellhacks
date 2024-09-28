1d548e611d20e2ab5bb37e77c4654268f12f9650
jordanzhancock <jordanzhancock@hotmail.com>
2024-09-28 15:11:21 -0400

    Added about view

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import Topbar from './Topbar';
 import Upload_Button from './Upload_Button';
 import Job_preview from './Job_preview';
+
+
 function App() {
   return (
-   <> 
-   <Topbar/>
-   <Upload_Button/>
-   <Job_preview/>
-   
- 
-   </>
+    <>
+      <Topbar />
+      <Upload_Button />
+      <Job_preview />
+    </>
+  
   );
 }
 
