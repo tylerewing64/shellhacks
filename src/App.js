e8b2d6c277d8224f915b43fbe2c73525e6c30ac1
tylerewing64 <76537101+tylerewing64@users.noreply.github.com>
2024-09-27 23:28:53 -0400

    All files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Topbar from './Topbar';
+function App() {
+  return (
+   <> 
+   <Topbar/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Topbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Topbar() {
+  return (
+  <>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <div className='top-container'>
+        <div name = 'icon-nav' className='flex-across full-width top-icons'> 
+            <span class="material-symbols-outlined cursor-pointer">person</span>
+            <div className='top-icons-right'>
+                <span class="material-symbols-outlined cursor-pointer">notifications</span>
+                <span class="material-symbols-outlined cursor-pointer">settings</span>
+            </div>
+        </div>
+        <div className='width-max'>
+            <h1 className= "cursor-pointer">Home</h1>
+            <h1 className= "cursor-pointer">Jobs</h1>
+            <h1 className= "cursor-pointer">About</h1>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Topbar
