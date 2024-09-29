7976b63af9d07be6f415b3e9a9018ee449eeb99f
tylerewing64 <76537101+tylerewing64@users.noreply.github.com>
2024-09-28 23:28:36 -0400

    Adding final changes

--- src/Upload_Button.js
+++ src/Upload_Button.js
@@ -17,7 +17,8 @@ function Upload_Button({setIndexOfComp, set_ai_gen_jobs}) {
       formDataObj.append('file', file);
       const response = await uploadFile(formDataObj);
       const data = await response.json();
-      set_ai_gen_jobs(data?.Jobs);
+      
+      set_ai_gen_jobs(data?.jobs);
       setIndexOfComp(3);
       
       
